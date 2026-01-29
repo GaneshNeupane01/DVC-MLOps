ff8e02f64e8d2850c43c19435617b560e6c7c64a
Ganesh Neupane <ganeshneupane1357@gmail.com>
2026-01-29 12:17:24 +0545

    first output with model output

--- preprocess.py
+++ preprocess.py
@@ -1,8 +1,9 @@
-with open("data/data.txt") as f:
-    nums = [int(x) for x in f]
+import os
+with open("large_files/demoOfLargefile.txt") as f:
+    split_words = f.read().split()
 
-processed = [x * 2 for x in nums]
-
+processed = [x.lower() for x in split_words]
+os.makedirs("data", exist_ok=True)
 with open("data/processed.txt", "w") as f:
     for x in processed:
         f.write(f"{x}\n")
--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 import yaml
-import random
+import random,os
 
 with open("params.yaml") as f:
     params = yaml.safe_load(f)
@@ -9,6 +9,11 @@ epochs = params["train"]["epochs"]
 
 accuracy = round(random.uniform(0.8, 0.99), 3)
 loss = round(1 - accuracy, 3)
+#simulate large modal outputs and save
+os.makedirs("output", exist_ok=True)
+text = "trained modal and this is demo large file content\n" * 100
+with open("output/model.txt", "w") as f:
+    f.write(text)
 
-with open("metrics.json", "w") as f:
+with open("output/metrics.json", "w") as f:
     f.write(f'{{"accuracy": {accuracy}, "loss": {loss}}}')
